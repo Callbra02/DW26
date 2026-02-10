8ae515d59f3c441bb29bf5eb06b8bc273494d4c1
000954560@mohawkcollege.ca <brandon.callaway.29@gmail.com>
2026-02-10 16:28:15 -0500

    Create interaction system Switch interaction handling based on ghost or player input

--- Assets/Scripts/bPlayerController.cs
+++ Assets/Scripts/bPlayerController.cs
@@ -8,19 +8,20 @@ public class bPlayerController : MonoBehaviour
     [field: SerializeField] public SpriteRenderer SpriteRenderer { get; private set; }
     [field: SerializeField] public Rigidbody2D Rigidbody2D { get; private set; }
     [field: SerializeField] public float MoveSpeed { get; private set; } = 10f;
-    [field: SerializeField] public float DashForce { get; private set; } = 5f;
-    [field: SerializeField] public float deccelSpeed { get; private set; } = 0.34f;
+    [field: SerializeField] public float DashSpeed { get; private set; } = 15f;
+    [field: SerializeField] public float deccelSpeed { get; private set; } = 2.5f;
     
     public bool isGhost = false;
     private bool canDash = true;
     private bool isDashing = false;
 
+    private float CurrentSpeed;
     public bool DoDash { get; private set; }
 
     // Player input information
     private PlayerInput PlayerInput;
     private InputAction InputActionMove;
-    private InputAction InputActionDash;
+    private InputAction InputActionInteract;
 
     // Assign color value on spawn from main spawner
     public void AssignColor(Color color)
@@ -35,8 +36,6 @@ public class bPlayerController : MonoBehaviour
             PlayerColor = color;
         }
         
-        
-
         // Assign to sprite renderer
         if (SpriteRenderer == null)
             Debug.Log($"Failed to set color to {name} {nameof(PlayerController)}.");
@@ -52,7 +51,9 @@ public class bPlayerController : MonoBehaviour
         // Find the references to the "Move" and "Jump" actions inside the player input's action map
         // Here I specify "Player/" but it in not required if assigning the action map in PlayerInput inspector.
         InputActionMove = playerInput.actions.FindAction($"Player/Move");
-        InputActionDash = playerInput.actions.FindAction($"Player/Dash");
+        
+        // Setup context bool for interact button
+        InputActionInteract = playerInput.actions.FindAction($"Player/Interact");
         playerInput.actions.FindAction("Player/Dash").started += ctx => isDashing = true;
         playerInput.actions.FindAction("Player/Dash").canceled += ctx => isDashing = false;
     }
@@ -66,13 +67,50 @@ public class bPlayerController : MonoBehaviour
     // Runs each frame
     public void Update()
     {
-        // Read the "Jump" action state, which is a boolean value
-        if (InputActionDash.WasPressedThisFrame() && canDash)
+        HandleDash();
+        
+        HandlePlayerInteract();
+        HandleGhostInteract();
+    }
+
+    void HandlePlayerInteract()
+    {
+        // Break if ghost
+        if (isGhost)
+            return;
+
+        if (InputActionInteract.WasPressedThisFrame())
         {
-            // Buffer input becuase I'm controlling the Rigidbody through FixedUpdate
-            // and checking there we can miss inputs.
-            DoDash = true;
-            //DoJump = true;
+            Debug.Log("Player Interact");
+        }
+    }
+
+    void HandleGhostInteract()
+    {
+        // Break if not ghost
+        if (!isGhost)
+            return;
+        
+        if (InputActionInteract.WasPressedThisFrame())
+        {
+            Debug.Log("Ghost Interact");
+        }
+    }
+    
+    // Updates player speed based on input and stamina
+    void HandleDash()
+    {
+        // Prevent dashing for ghosts
+        if (isGhost)
+            return;
+        
+        if (isDashing)
+        {
+            CurrentSpeed = DashSpeed;
+        }
+        else
+        {
+            CurrentSpeed = MoveSpeed;
         }
     }
 
@@ -89,31 +127,16 @@ public class bPlayerController : MonoBehaviour
         // Read the "Move" action value, which is a 2D vector
         Vector2 moveValue = InputActionMove.ReadValue<Vector2>();
         // Here we're only using the X axis to move.
-        float moveForce = moveValue.x * MoveSpeed;
-        float moveForceY = moveValue.y * MoveSpeed;
+        float moveForce = moveValue.x * CurrentSpeed;
+        float moveForceY = moveValue.y * CurrentSpeed;
         // Apply fraction of force each frame
         Rigidbody2D.AddForceX(moveForce, ForceMode2D.Force);
         Rigidbody2D.AddForceY(moveForceY, ForceMode2D.Force);
 
         if (Rigidbody2D.linearVelocity != Vector2.zero)
         {
-            
             Vector2 deccelVel = Vector2.Lerp(Rigidbody2D.linearVelocity, Vector2.zero, Time.deltaTime * deccelSpeed);
-            
-            string msg = $"LVP: {Rigidbody2D.linearVelocity}, LVA: {deccelVel}";
-            Debug.Log(msg);
             Rigidbody2D.linearVelocity = deccelVel;
-        }
-
-        // Dash only if the player is not the ghost
-        if (DoDash && !isGhost)
-        {
-            // Get current velocity
-            Vector3 currentVelocity = Rigidbody2D.linearVelocity;
-            currentVelocity *= DashForce;
-            
-            Rigidbody2D.AddForce(currentVelocity, ForceMode2D.Impulse);
-            DoDash = false;
         }
     }
 
